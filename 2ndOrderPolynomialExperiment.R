ca4e122ca799cd11bf73d3ab746589c1ea281bde
Dmitry A. Grechka <dmitry.grechka@gmail.com>
2016-03-04 13:42:30 +0300

    2nd order piecewise poly approx experiment is ready

--- 2ndOrderPolynomialExperiment.R
+++ 2ndOrderPolynomialExperiment.R
@@ -1,12 +1,11 @@
 obs <- read.csv('SampleData/observations.csv', header = T, sep = ';')
 colnames(obs) <- c('t','v')
-#plot(obs$t,obs$v)
 
 step <- 25
+left <-0
+right <- 650
 
-nodes <- seq(from=0, to=650, by=step)
-
-p <- numeric((length(nodes)-1)*3)
+nodes <- seq(from=left, to=right, by=step)
 
 poly_f <-function(p,step,t) {
   i <- t %/% step
@@ -34,15 +33,49 @@ getP <- function(initSlope,X,Y) {
   return(res)
 }
 
-testX = c(0,5,10,15,20)
-testY = c(10,5,5,12,14)
-
-p1 <-getP(-3,testX,testY)
-
-
-
-poly_f1 <- function(x) {
-  return(poly_f(p1,5,x))
+poly_fitted <- function(initSlope,X,Y,step) {
+  p1 <- getP(initSlope,X,Y)
+  
+  return(function(x) {
+    poly_f(p1,step,x)
+    }
+  );
 }
-plot(testX,testY,ylim=c(0,20))
-curve(poly_f1,add = T, from = 0, to = 20)
+
+testInitSlope <- 0
+
+toMinimize <- function(x) {
+  sqErr <- 0
+  cur_x_fitted <- poly_fitted(testInitSlope,nodes,x,step)
+  for(j in 1:nrow(obs)){
+    dif1 <- cur_x_fitted(obs$t[j])-obs$v[j]
+    if(! is.na(dif1))
+      sqErr <- sqErr + dif1*dif1
+  }
+  return(sqErr)
+}
+
+init <- rep(400, length(nodes))
+
+opt_res1 <- optim(init,toMinimize, , method = "BFGS",control = list(maxit = 500))
+
+plot(obs$t,obs$v,
+     xlab = "Time (hours since 03.02.2016  18:00:00)",
+     ylab="Velocity (km/s)",
+     main = "Solar Wind near Earth\n2nd order piecewise polynomial approximation of observation data")
+
+res1_poly <- poly_fitted(testInitSlope,nodes,opt_res1$par,step)
+curve(res1_poly,add = T, from = 0, to = 650)
+
+#testX = c(0,5,10,15,20)
+#testY = c(10,5,5,12,14)
+
+#p1 <-getP(-3,testX,testY)
+
+
+
+#poly_f1 <- function(x) {
+#  return(poly_f(p1,5,x))
+#}
+#plot(testX,testY,ylim=c(0,20))
+#curve(poly_f1,add = T, from = 0, to = 20)
