21ea14bb827a3ac47eb061aebaa6c9411276421e
Dmitry A. Grechka <b-dmgrec@microsoft.com>
2016-03-18 14:57:34 +0300

    HTML.Save does not work yet

---
+++ 2.VisualInspectionOfTimesries.fsx
@@ -0,0 +1,14 @@
+﻿System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+#I @".NET\PulseEngineApp\bin\Debug"
+#r "Angara.table.dll"
+#r "Angara.Statistics.dll"
+#r "Angara.Html.dll"
+
+open Angara
+open Angara.Data
+open Angara.Statistics
+
+let ch_data = Table.Load("1.ChAreaApproximated.csv")
+
+Html.Save "Observations.html" ch_data
